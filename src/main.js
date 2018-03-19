5396b5ff10217e646fc5974708fdafee85341be1
SykoKin <1037671280@qq.com>
2018-03-19 18:38:05 +0800

    vue-router for vue2

--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import App from './App.vue';
 import VueRouter from 'vue-router';
 import goods from './components/goods/goods';
+import seller from './components/seller/seller';
+import ratings from './components/ratings/ratings';
+
+import './common/stylus/index.styl';
 
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
-let app = Vue.extend(App);
+const routes = [
+  {path: '/', component: goods},
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
+];
 
-let router = new VueRouter();
-
-router.map({
-  '/goods': {
-    component: goods
-  }
+const router = new VueRouter({
+  routes
 });
 
-router.start(app, '#app');
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router: router,
+  template: '<App/>', // 指定需要挂载的组件
+  components: {// 注册组件
+    App: App
+  }
+});
