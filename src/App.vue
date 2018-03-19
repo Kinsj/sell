ec8e45b7d57e9a4f7e06be6a2a458c4ac56ec644
SykoKin <1037671280@qq.com>
2018-03-19 22:27:20 +0800

    header frame

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header> <!--:seller 是v-bind: seller...的缩写-->
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -16,10 +16,26 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import header from './components/header/header.vue';
 
+  const ERR_OK = 0;
+
   export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    created() { // 生命周期钩子，created钩子
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
     components: {
       'v-header': header
     }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import goods from './components/goods/goods';
 import seller from './components/seller/seller';
 import ratings from './components/ratings/ratings';
@@ -10,6 +11,7 @@ import ratings from './components/ratings/ratings';
 import './common/stylus/index.styl';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 Vue.config.productionTip = false;
 
